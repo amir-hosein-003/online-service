4a5af21041e0d8de2f922054db57a58b9a95adda
amir-hosein-003 <ah.kanani003@gmail.com>
2025-12-01 16:40:51 +0330

    add admin dashboard subRoutes

---
+++ app/(admin)/admin/settings/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SettingsPage = () => {
+  return <div className="text-3xl font-bold">SettingsPage</div>;
+};
+
+export default SettingsPage;
---
+++ app/(admin)/admin/users/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div className="text-3xl font-bold">UsersPage</div>
+  )
+}
+
+export default UsersPage
